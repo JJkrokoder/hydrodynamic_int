f1a2b09c7c0d6e63a1ea6a34bfb21b69b2f86191
Joan Javier Ronquillo Tutiven <joan@ronquillo.ftmc.uam.es>
2025-02-28 13:07:56 +0100

    Creados init, función de cálculo del tensor de movilidad y prueba de obtención para self mobility. Queda documentar y comprobar la forma matricial teniendo en cuenta la estructura de los arrays

---
+++ test/examples/obtain_SF_tensor.py
@@ -0,0 +1,32 @@
+import numpy as np
+from libMobility import SelfMobility
+from utils.getMobilityTensor import getMobilityTensor
+
+# Define the number of particles in the system
+numberParticles = 2
+# Set calculation precision based on solver's precision
+precision = np.float64
+# STEP 1: Create solver object with open boundary conditions in all three dimensions
+solver = SelfMobility("open", "open", "open")
+# STEP 2: Configure specific solver parameters
+solver.setParameters(parameter=5)
+# STEP 3: Initialize solver with global parameters
+solver.initialize(
+  temperature=0.0,
+  viscosity=1.0,
+  hydrodynamicRadius=1.0,
+  numberParticles=numberParticles,
+  needsTorque=False,
+)
+
+# Create random positions for particles
+positions = np.random.rand(numberParticles, 3).astype(precision)
+
+# Obtain the mobility tensor
+mobilitytensor=getMobilityTensor(positions, solver)
+
+print(mobilitytensor)
+
+# Theoretical Tensor
+theoreticalTensor = np.diag([1 / (6 * np.pi)] * (3 * numberParticles))
+print(theoreticalTensor)
--- test/utils/getMobilityTensor.py
+++ test/utils/getMobilityTensor.py
@@ -4,16 +4,16 @@ def getMobilityTensor(positions, solver):
     # Check if the solver is initialized
 
     # Check the dimensions of the positions array
-    if len(positions) != solver.numberParticles*3:
-        raise ValueError("Number of particles in positions array does not match the solver.")
 
+    numberparticles = positions.shape[0]
+    print(numberparticles)
     # Perform the algorithm to obtain the mobility tensor
-    mobility_tensor = np.zeros((solver.numberParticles*3, solver.numberParticles*3))
-    for i in range(solver.numberParticles*3):
-        force = np.zeros(solver.numberParticles*3)
-        force[i] = 1
-        velocity = solver.Mdot(force)
-        mobility_tensor[:, i] = velocity
+    mobility_tensor = np.zeros((numberparticles*3, numberparticles*3))
+    for i in range(numberparticles*3):
+        force = np.zeros((numberparticles, 3))
+        force[i//3,i-3*(i//3)] = 1
+        velocity = solver.Mdot(force)[0]
+        mobility_tensor[:, i] = velocity.reshape(-1, 1).flatten()
 
     # Return the mobility tensor as a matrix
     return mobility_tensor
