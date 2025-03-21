0c19635cc0fdea26f301c2e862816c96a19c4d3d
Joan Javier Ronquillo Tutiven <joan@ronquillo.ftmc.uam.es>
2025-03-21 11:33:54 +0100

    Cheanged into a .toml package intallation file and added information about the modules

--- src/hydrodynamic_int/__init__.py
+++ src/hydrodynamic_int/__init__.py
@@ -1,1 +1,10 @@
 from .utils import getMobilityTensor, getMobilityTensorRPY
+
+__all__ = [
+    'getMobilityTensor',
+    'getMobilityTensorRPY'
+]
+
+__version__ = '0.1.0'
+__author__ = 'Joan Ronquillo'
+
