43ef3851be5437e0c1ae84acd0160a6b71800aea
Joan Javier Ronquillo Tutiven <joan@ronquillo.ftmc.uam.es>
2025-03-24 21:47:13 +0100

    Added particles properties testing

--- test/test_particles.py
+++ test/test_particles.py
@@ -11,4 +11,13 @@ def test_particles():
     # Check the positions
     assert np.array_equal(particles.positions, np.array([[0.0, 0.0, 0.0]]))
     
+    # Test the creation of a Particles object with additional properties
+    labels = ['id', 'position', 'mass', 'charge']
+    data = [0, np.array([0.0, 0.0, 0.0]), 1.0, -1.0]
+    particles = Particles(labels, data)
+    # Check the additional properties
+    assert particles.properties['mass'] == 1.0
+    assert particles.properties['charge'] == -1.0
+
+    
     
