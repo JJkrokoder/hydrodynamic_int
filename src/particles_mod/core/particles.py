2a2cb1ff785017a5ff45381ea1179aaddad2d0df
Joan Javier Ronquillo Tutiven <joan@ronquillo.ftmc.uam.es>
2025-03-25 10:32:06 +0100

    Updated data parameter and arguments initialization in particle's class. Tests passed. Ids mapping pending

--- src/particles_mod/core/particles.py
+++ src/particles_mod/core/particles.py
@@ -13,49 +13,45 @@ class Particles:
     labels : list
         List containing the labels of the properties of the particles (ids and positions are mandatory).
     data: list
-        List containing the data of the particles (ids and positions are mandatory).
+        List containing the data lists of the particles (ids and positions are mandatory).
     
     Attributes
     ----------
+    labels : list
+        List containing the labels of the properties of the particles.
     ids : numpy.ndarray
         Array containing the ids of the particles.
     positions : numpy.ndarray
         Array containing the positions of the particles.
-    properties : dict
-        Dictionary containing optional properties of the particles.
+    properties : numpy.ndarray
+        Array containing any other optional property of the particles.
     
     Methods
     -------
     '''
 
-    def __init__(self, labels : list, data : np.ndarray):
+    def __init__(self, labels : list, data : list[list]):
         '''
         Constructor of the Particles class.
         '''
-        # Check if the labels and data have the same length
-        if len(labels) != len(data):
-            raise ValueError('The labels and data must have the same length.')
         # Check if the labels contain the mandatory labels
-        if 'id' not in labels:
-            raise ValueError('The labels must contain the id label.')
-        if 'position' not in labels:
-            raise ValueError('The labels must contain the position label.')
+        if 'id' not in labels or 'position' not in labels:
+            raise ValueError("'id' and 'position' are mandatory labels.")
         
-        # Check that the first element is an integer id and the second element is a position array
-        if not isinstance(data[0], int):
-            raise ValueError('The first element of the data must be an integer id.')
-        if not isinstance(data[1], np.ndarray):
-            raise ValueError('The second element of the data must be a position array.')
+        # Check if the labels and each particle data have the same length for every particle
+        data_lengths = np.array([len(d) for d in data])
+        if not np.all(data_lengths == len(labels)):
+            # The error message shows the id corresponding to the particle with the wrong length
+            wrong_length_id = np.where(data_lengths != len(labels))[0][0]
+            raise ValueError(f'The labels and data must have the same length. Error for particle with id {data[wrong_length_id][0]}')
         
-        # Initialize the ids, positions and properties
-        self.ids = np.array([data[0]])
-        self.positions = np.array([data[1]])
-        self.properties = {}
-        # Add the rest of the data to the properties
-        for i in range(2, len(labels)):
-            self.properties[labels[i]] = data[i]
-        
-        
+        # Create the attributes of the class
+        self.labels = labels
+        for label in labels:
+            setattr(self, label, np.array([d[labels.index(label)] for d in data]))
 
+
+    
+                
 
         
--- test/test_particles.py
+++ test/test_particles.py
@@ -2,22 +2,69 @@ import numpy as np
 from particles_mod.core import Particles
 
 def test_particles():
+    
+    # Check if the error is raised when any of the particles data have different lengths than the labels
+    labels = ['id', 'position']
+    data = [[3, np.array([0.0, 0.0, 0.0]), 1.0], [1, np.array([1.0, 1.0, 1.0])]]
+    try:
+        particles = Particles(labels, data)
+    except ValueError as e:
+        assert str(e) == 'The labels and data must have the same length. Error for particle with id 3'
+    ####################################################################################################
+
+    # Check if the error is raised when the labels do not contain the mandatory labels
+    labels = ['position']
+    data = [0]
+    try:
+        particles = Particles(labels, data)
+    except ValueError as e:
+        assert str(e) == "'id' and 'position' are mandatory labels."
+    ####################################################################################################
+
     # Test the creation of a Particles object
     labels = ['id', 'position']
-    data = [0, np.array([0.0, 0.0, 0.0])]
+    data = [[0, np.array([0.0, 0.0, 0.0])],
+            [1, np.array([1.0, 1.0, 1.0])],
+            [2, np.array([2.0, 2.0, 2.0])]]
     particles = Particles(labels, data)
     # Check the ids
-    assert np.array_equal(particles.ids, np.array([0]))
+    assert np.all(particles.id == np.array([0, 1, 2]))
     # Check the positions
-    assert np.array_equal(particles.positions, np.array([[0.0, 0.0, 0.0]]))
-    
+    assert np.all(particles.position == np.array([[0.0, 0.0, 0.0], [1.0, 1.0, 1.0], [2.0, 2.0, 2.0]]))
+    ####################################################################################################
+
     # Test the creation of a Particles object with additional properties
-    labels = ['id', 'position', 'mass', 'charge']
-    data = [0, np.array([0.0, 0.0, 0.0]), 1.0, -1.0]
+    labels = ['id', 'position', 'property', 'material']
+    data = [[0, np.array([0.0, 0.0, 0.0]), 1.0, 'A'],
+            [1, np.array([1.0, 1.0, 1.0]), 2.0, 'B'],
+            [2, np.array([2.0, 2.0, 2.0]), 3.0, 'C']]
     particles = Particles(labels, data)
-    # Check the additional properties
-    assert particles.properties['mass'] == 1.0
-    assert particles.properties['charge'] == -1.0
+    # Check the ids
+    assert np.all(particles.id == np.array([0, 1, 2]))
+    # Check the positions
+    assert np.all(particles.position == np.array([[0.0, 0.0, 0.0], [1.0, 1.0, 1.0], [2.0, 2.0, 2.0]]))
+    # Check the properties
+    assert np.all(particles.property == np.array([1.0, 2.0, 3.0]))
+    # Check the materials
+    assert np.all(particles.material == np.array(['A', 'B', 'C']))
+    ####################################################################################################
+
+    # Test the creation of a Particles object with additional properties and different order
+    labels = ['id', 'material', 'position', 'property']
+    data = [[0, 'A', np.array([0.0, 0.0, 0.0]), 1.0],
+            [1, 'B', np.array([1.0, 1.0, 1.0]), 2.0],
+            [2, 'C', np.array([2.0, 2.0, 2.0]), 3.0]]
+    particles = Particles(labels, data)
+    # Check the ids
+    assert np.all(particles.id == np.array([0, 1, 2]))
+    # Check the positions
+    assert np.all(particles.position == np.array([[0.0, 0.0, 0.0], [1.0, 1.0, 1.0], [2.0, 2.0, 2.0]]))
+    # Check the properties
+    assert np.all(particles.property == np.array([1.0, 2.0, 3.0]))
+    # Check the materials
+    assert np.all(particles.material == np.array(['A', 'B', 'C']))
+    ####################################################################################################
+    
 
     
     
