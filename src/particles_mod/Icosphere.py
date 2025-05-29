a9bc770ca6f6c881fcc42e05431494e4568ca38c
Joan Javier Ronquillo Tutiven <joan@ronquillo.ftmc.uam.es>
2025-05-29 15:49:27 +0200

    finished first version of icosphere class and tests

--- src/particles_mod/Icosphere.py
+++ src/particles_mod/Icosphere.py
@@ -1,6 +1,10 @@
 import icosphere as ico
 import numpy as np
+import VLMP
 from typing import Iterable
+import tempfile
+import json
+
 
 class IcoSphere:
     '''
@@ -14,33 +18,118 @@ class IcoSphere:
         The surface density of particles in the icosphere.
     Kpair :
         Spring constant for the pair bonds.
-    Kangle :
-        Spring constant for the angular bonds.
+    Kdi :
+        Spring constant for the dihedral bonds
     '''
 
-    def __init__(self, radius: float = 1.0, density: float = 1.0, Kpair: float = 1.0, Kangle: float = 1.0):
+    def __init__(self, radius: float = 1.0, density: float = 1.0, Kpair: float = 1.0, Kdi: float = 1.0):
         self.radius = radius
         self.Kpair = Kpair
-        self.Kangle = Kangle
+        self.Kdi = Kdi
 
         nparticles = round(4 * np.pi * radius**2 * density)
-        self.nu = round(np.sqrt(1 + (nparticles - 12)/10))
+        self.freq_division = round(np.sqrt(1 + (nparticles - 12)/10))
 
-        normalized_positions, self.faces = ico.icosphere(self.nu)
+        normalized_positions, self.faces = ico.icosphere(self.freq_division)
         self.positions = normalized_positions * radius
         self.nparticles = len(self.positions)
         self.density = nparticles / (4 * np.pi * radius**2)
+    
 
-    def generate_positions(self) -> Iterable[float]:
+    
+    def _generate_data(self) :
         """
-        Generates the positions of the particles in the icosphere structure.
+        Generate a UAMMD-structured data object for the icosphere.
 
-        Returns
-        -------
-        positions :
-            Positions of the icosphere particles.
+        Parameters
+        ----------
+        path :
+            The path where the JSON file will be saved.
         """
-        return self.positions.tolist()
+
+        temp_file = "current_state"
+        simulationPool = [{
+            "system": [
+                {"type": "simulationName", "parameters": {"simulationName": "Icosphere"}}
+            ],
+            "units": [{"type": "none"}],
+            "types": [{"type": "basic"}],
+            "ensemble": [
+                {"type": "NVT", "parameters": {"box": [100.0, 100.0, 100.0],
+                                            "temperature": 0.0}}
+            ],
+            "integrators": [
+                {"type": "BBK", "parameters": {"timeStep": 0.0,
+                                            "frictionConstant": 0.0,
+                                            "integrationSteps": 1}}
+            ],
+            "models": [
+                {"type": "ICOSPHERE", "parameters": {
+                "resolution": self.freq_division,
+                "radius": self.radius,
+                "particleName": "S",
+                "particleRadius": 0.5,
+                "Kb": self.Kpair,
+                "Kd": self.Kdi,
+                "steric": False
+                }},
+            ],
+            "simulationSteps": [
+                {"type": "saveState", "parameters": {"intervalStep": 0,
+                                                    "outputFilePath": temp_file,
+                                                    "outputFormat": "xyz"}},
+            ]
+        }]
+
+        # Initialize VLMP and load simulation pool
+        vlmp = VLMP.VLMP()
+        vlmp.loadSimulationPool(simulationPool)
+
+        # Distribute simulations and set up
+        vlmp.distributeSimulationPool("size", 1)
+
+        with tempfile.TemporaryDirectory() as dir:
+            vlmp.setUpSimulation(dir)
+
+            json_path = f"{dir}/simulationSets/simulationSet_0/Icosphere/simulation.json"
+
+            with open(json_path, 'r') as json_file:
+                data = json.load(json_file)
+
+        return data
+
+def construct_structure(radius: float = 1.0, density: float = 1.0, Kpair: float = 1.0, Kdi: float = 1.0) -> tuple:
+    """
+    Constructs the structure of the icosphere and generates the positions and bonds.
+
+    Parameters
+    ----------
+    radius :
+        The radius of the icosphere.
+    density :
+        The surface density of particles in the icosphere.
+    Kpair :
+        Spring constant for the pair bonds.
+    Kdi :
+        Spring constant for the dihedral bonds.
+
+    Returns
+    -------
+    positions :
+        Positions of the icosphere particles.
+    bonds :
+        Dictionary containing the pair bonds.
+    """
+    
+    icosphere = IcoSphere(radius=radius, density=density, Kpair=Kpair, Kdi=Kdi)
+    vlmp_data = icosphere._generate_data()
+    
+    positions = vlmp_data['state']['data']
+    positions = [[pos[1]] for pos in positions]
+
+    bonds = vlmp_data['topology']['forceField']['bonds']
+
+    return positions, bonds
 
 
 
--- test/test_icosphere.py
+++ test/test_icosphere.py
@@ -1,5 +1,7 @@
 import particles_mod.Icosphere as ico
 import numpy as np
+import tempfile
+import os
 
 def test_icosphere():
     
@@ -13,7 +15,7 @@ def test_icosphere():
 
     assert icosphere.radius == r, f"Expected radius {r}, got {icosphere.radius}"
     assert icosphere.Kpair == Kpair, f"Expected Kpair {Kpair}, got {icosphere.Kpair}"
-    assert icosphere.Kangle == 1.0, f"Expected Kangle 1.0, got {icosphere.Kangle}"
+    assert icosphere.Kdi == 1.0, f"Expected Kdi 1.0, got {icosphere.Kdi}"
 
     assert np.isclose(icosphere.nparticles, nparticles, rtol=1e-1), f"Expected nparticles {nparticles}, got {icosphere.nparticles}"
     assert np.isclose(icosphere.density, d, rtol=1e-2), f"Expected density {d}, got {icosphere.density}"
@@ -21,20 +23,60 @@ def test_icosphere():
     distances = np.linalg.norm(icosphere.positions, axis=1)
     assert np.all(np.isclose(distances, r, atol=1e-12)), "Not all particles are at the expected radius"
 
-def test_icosphere_positions():
-    r = 20.0
-    d = 4.0
+def test_JSONgeneration():
+    '''
+    Test the JSON generation of the IcoSphere class
+    '''
 
-    icosphere = ico.IcoSphere(radius=r, density=d)
-    positions = icosphere.generate_positions()
+    r = 2.0
+    d = 3.0
+    Kpair = 1.5
+    icosphere = ico.IcoSphere(radius=r, density=d, Kpair=Kpair)
 
-    assert isinstance(positions, list), "Positions should be a list"
-    assert all(isinstance(pos, list) for pos in positions), "Each position should be a list"
+    data = icosphere._generate_data()
 
-    assert len(positions) == icosphere.nparticles, f"Expected {icosphere.nparticles} positions, got {len(positions)}"
+    assert isinstance(data, dict), "Data should be a dictionary"
+    assert 'topology' in data, "Data should contain 'topology' key"
+    assert 'state' in data, "Data should contain 'state' key"
+    assert 'system' in data, "Data should contain 'system' key"
+    assert 'global' in data, "Data should contain 'global' key"
+    assert 'integrator' in data, "Data should contain 'integrator' key"
+    assert 'simulationStep' in data, "Data should contain 'simulationStep' key"
     
-    for pos in positions:
-        assert len(pos) == 3, f"Position {pos} does not have 3 coordinates"
-        assert np.isclose(np.linalg.norm(pos), r, atol=1e-12), f"Position {pos} is not at the expected radius {r}"
+    nparticles_obtained = len(data['state']['data'])
+    assert nparticles_obtained == icosphere.nparticles, f"Expected {icosphere.nparticles} particles, got {nparticles_obtained}"
+    for i in range(icosphere.nparticles):
+        assert len(data['state']['data'][i]) == 2, f"Particle {i} data should have 2 elements, got {len(data['state']['data'][i])}"
+        assert len(data['state']['data'][i][1]) == 3, f"Particle {i} position should have 3 coordinates, got {len(data['state']['data'][i][1])}"
+
+    positions_array = np.array([data['state']['data'][i][1] for i in range(icosphere.nparticles)])
+    assert np.all(np.isclose(np.linalg.norm(positions_array, axis=1), r, atol=1e-12)), "Not all particles are at the expected radius in JSON data"
+
+    assert positions_array.shape == (icosphere.nparticles, 3), f"Expected positions array shape {(icosphere.nparticles, 3)}, got {positions_array.shape}"
+    
+    assert np.all(np.isclose(icosphere.positions, positions_array)), "Positions in JSON data do not match IcoSphere positions"
+                  
+
+def test_construct_structure():
+    """
+    Test the construct_structure function with a known configuration.
+    """
+    radius = 2.0
+    density = 3.0
+    Kpair = 1.0
+    Kdi = 1.0
+
+    nparticles = round(4 * np.pi * radius**2 * density)
+    freq_division = round(np.sqrt(1 + (nparticles - 12)/10))
+    nparticles = 12 + 10 *(freq_division**2 - 1)
+
+    positions, bonds = ico.construct_structure(radius = radius, density = density, Kpair=Kpair, Kdi=Kdi)
+
+    assert len(positions) == nparticles, f"Expected {nparticles} particles, got {len(positions)}"
+    assert isinstance(bonds, dict), "Bonds should be a dictionary"
 
 
+    
+    
+    
+      
